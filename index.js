7bee24c6c7ca3d69f87e5ce1212f7dd6e13e6fd2
joshua-dyer <145073496+joshua-dyer@users.noreply.github.com>
2023-10-11 18:36:09 -0500

    Add files via upload
    This is one of my first JavaScript projects.  Its intention is to provide a simple solution when my RPG group forgets to bring dice, or perhaps we sit down for an unplanned game!  Lots of styling work left to be done.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+let d6;
+let d8;
+let d10;
+let d20;
+
+document.getElementById("rollD6Button").onclick = function(){
+
+    d6 = (Math.floor((Math.random()) *6) + 1);
+
+    document.getElementById("resultLabelD6").innerHTML = d6;
+}
+
+document.getElementById("rollD8Button").onclick = function(){
+
+    d8 = (Math.floor((Math.random()) *8) + 1);
+
+    document.getElementById("resultLabelD8").innerHTML = d8;
+}
+
+document.getElementById("rollD10Button").onclick = function(){
+
+    d10 = (Math.floor((Math.random()) *10) + 1);
+
+    document.getElementById("resultLabelD10").innerHTML = d10;
+}
+
+document.getElementById("rollD20Button").onclick = function(){
+
+    d20 = (Math.floor((Math.random()) *20) + 1);
+
+    document.getElementById("resultLabelD20").innerHTML = d20;
+}
