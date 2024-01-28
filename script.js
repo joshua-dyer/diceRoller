22d4dc212f811755086f95437ba0bafc50a6e809
joshua-dyer <145073496+joshua-dyer@users.noreply.github.com>
2024-01-28 09:52:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,26 +8,26 @@ document.getElementById("rollD6Button").onclick = function(){
 
     d6 = (Math.floor((Math.random()) *6) + 1);
 
-    document.getElementById("resultLabelD6").innerHTML = d6;
+    document.getElementById("resultLabelD6").innerHTML += d6;
 }
 
 document.getElementById("rollD8Button").onclick = function(){
 
     d8 = (Math.floor((Math.random()) *8) + 1);
 
-    document.getElementById("resultLabelD8").innerHTML = d8;
+    document.getElementById("resultLabelD8").innerHTML += d8;
 }
 
 document.getElementById("rollD10Button").onclick = function(){
 
     d10 = (Math.floor((Math.random()) *10) + 1);
 
-    document.getElementById("resultLabelD10").innerHTML = d10;
+    document.getElementById("resultLabelD10").innerHTML += d10;
 }
 
 document.getElementById("rollD20Button").onclick = function(){
 
     d20 = (Math.floor((Math.random()) *20) + 1);
 
-    document.getElementById("resultLabelD20").innerHTML = d20;
+    document.getElementById("resultLabelD20").innerHTML += d20;
 }
